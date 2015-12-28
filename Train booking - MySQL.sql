155623db906c7c6563efb6dc4721e8ed9929deb7
amrzohdi <amr.essam67@stud.fci-cu.edu.eg>
2015-12-28 22:43:22 +0200

    fix merging

--- Train booking - MySQL.sql
+++ Train booking - MySQL.sql
@@ -19,13 +19,14 @@ Create Table User
 Create Table Trip
 (
   Name varchar(100) primary key,
+  TrainId INT REFERENCES Train(Id),
   Source varchar(100) NOT NULL,
   Destination Varchar(100) NOT NULL,
   StartTime TIMESTAMP NOT NULL,
   EndTime TIMESTAMP NOT NULL,
-  Capacity INT NOT NULL,
+  Price INT NOT NULL,
   Reserved INT DEFAULT 0,
-  Price INT 
+  Capacity INT NOT NULL,
 );
 -- Registered(UserName , TripName)
 Create Table Registered
@@ -33,4 +34,11 @@ Create Table Registered
   UserName varchar(100) REFERENCES User(UserName),
   TripName varchar(100) REFERENCES Trip(Name),
   primary key (UserName, TripName)
+);
+-- Train(Id, Capacity, Type), Capacity is the number of seats available in the Train.
+Create Table Train
+(
+  Id INT primary key,
+  Capacity INT NOT NULL,
+  Type varchar(100) NOT NULL
 );
